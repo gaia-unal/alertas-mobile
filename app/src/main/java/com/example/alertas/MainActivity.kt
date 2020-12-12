971a539e01b6271d3ed82df3c922b0f5e8d976bc
Orlando Hernandez <ohernandezn@unal.edu.co>
2020-12-11 22:13:48 -0500

    Connect with backend rest services

--- app/src/main/java/com/example/alertas/MainActivity.kt
+++ app/src/main/java/com/example/alertas/MainActivity.kt
@@ -4,23 +4,33 @@ import android.os.Bundle
 import android.os.RemoteException
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.google.gson.Gson
+import com.google.gson.JsonParser
 import kotlinx.android.synthetic.main.activity_main.*
 import org.altbeacon.beacon.*
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.concurrent.schedule
+import kotlin.concurrent.scheduleAtFixedRate
 
 data class DetectedBeacon(
     val id: String,
-    val from: String,
+    val from_: String,
     var until: String,
     val rssi: MutableList<Int>
 )
 
 class MainActivity : AppCompatActivity(), BeaconConsumer {
     private var beaconManager: BeaconManager? = null
-    private var detectedBeacons: MutableMap<String, DetectedBeacon> = mutableMapOf<String, DetectedBeacon>()
+    private var detectedBeacons: MutableList<DetectedBeacon> = mutableListOf<DetectedBeacon>()
     private val TAG = "DetectedBeacons"
+    private var sessionId: String? = null
+    val serverAPIURL = "http://74f6d16856cb.ngrok.io/"
+    private val timer = Timer("schedule", true)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,22 +50,24 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
 
                 beacons.forEach { beacon ->
                     val beaconId = beacon.id2.toString()
+                    val detectedBeacon = detectedBeacons.firstOrNull{ it.id == beaconId }
 
-                    if (!detectedBeacons.containsKey(beaconId)) {
+                    if (detectedBeacon == null) {
                         val c = Calendar.getInstance()
                         val sdf = SimpleDateFormat(
                             "yyyy-MM-dd HH:mm:ss.SSS",
                             Locale.US
                         )
-                        detectedBeacons[beaconId] = DetectedBeacon(beaconId, sdf.format(c.time), sdf.format(c.time), arrayListOf(beacon.rssi))
+
+                        detectedBeacons.add(DetectedBeacon(beaconId, sdf.format(c.time), sdf.format(c.time), arrayListOf(beacon.rssi)))
                     } else {
                         val c = Calendar.getInstance()
                         val sdf = SimpleDateFormat(
                             "yyyy-MM-dd HH:mm:ss.SSS",
                             Locale.US
                         )
-                        detectedBeacons[beaconId]?.until = sdf.format(c.time)
-                        detectedBeacons[beaconId]?.rssi?.add(beacon.rssi)
+                        detectedBeacon.until = sdf.format(c.time)
+                        detectedBeacon.rssi.add(beacon.rssi)
                     }
                 }
 
@@ -64,11 +76,35 @@ class MainActivity : AppCompatActivity(), BeaconConsumer {
                 runOnUiThread {
                     info.text = json.toString()
                 }
-            } /*else {
-                runOnUiThread {
-                    info.text = json.
+
+                if (sessionId == null) {
+                    Fuel.post(serverAPIURL).jsonBody(json).also {
+                        Log.i(TAG, it.toString())
+                    }.response { result ->
+                        val (bytes, error) = result
+
+                        if (bytes != null) {
+                            val e = JsonParser().parse(String(bytes))
+                            val obj = e.asJsonObject
+                            sessionId = obj.get("session_uuid").asString
+
+                            timer.scheduleAtFixedRate(30000, 30000)
+                            {
+                                Fuel.patch(serverAPIURL + "beacons/" + sessionId).jsonBody(Gson().toJson(detectedBeacons)).also{
+                                    Log.i(TAG, it.toString())
+                                }.response{ result -> }
+                            }
+
+                            runOnUiThread {
+                                info.text = "Accede a los datos en http://gaia.manizales.unal.edu.co/alertas/beacons/" + sessionId
+                            }
+
+                            Log.i(TAG, obj.toString())
+                        }
+                    }
+
                 }
-            }*/
+            }
         }
         try {
             beaconManager!!.startRangingBeaconsInRegion(Region("myRangingUniqueId", myBeaconNamespaceId, null, null))
